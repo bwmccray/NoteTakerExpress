99739bb77d8b33cd026bcd00f381eec58e8014ae
Brianna McCray <brianna.mccray@outlook.com>
2020-01-24 09:46:19 -0600

    Fixed CSS

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
-const db = require('../db/db.json');
+const db = require('./db/db.json');
+
 
 //establishing express
 var app = express();
+app.use(express.static('public'));
+
 //establish PORT
 var PORT = process.env.PORT || 3000;
+
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -17,12 +21,12 @@ app.use(express.json());
 //==================================================================================================
 //telling the application to return 'index.html' when the path has a '/'
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 //telling the application to return 'notes.html' when the path has a '/notes'
 app.get('/notes', function(req, res) {
-    res.sendFile(path.join(__dirname, 'notes.html'));
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 //===================================================================================================
 
@@ -39,7 +43,7 @@ app.post('/api/notes', function(req, res) {
 
     console.log(newNote);
 
-    characters.push(newNote);
+    db.push(newNote);
 
     res.json(newNote);
 })
